9dcd25952ee3d7368b6c8a91659b648646796805
danielred007 <danielrojo1927@gmail.com>
2024-01-08 20:01:38 +0000

    radio box component started

--- lab-01/__tests__/pages/reservations.test.tsx
+++ lab-01/__tests__/pages/reservations.test.tsx
@@ -1,10 +1,7 @@
 import { renderComponent } from '../testUtils/renderComponent'
-import { render, screen } from '@testing-library/react'
-import { makeStore } from '../../src/app/lib/store'
 import Page from '../../src/app/reservations/page'
-import { Provider } from 'react-redux'
+import { screen } from '@testing-library/react'
 import '@testing-library/jest-dom'
-import { ReactNode } from 'react'
 
 describe('Reservations Page Test Suite', () => {
   it('renders a heading', () => {
