f0af60ca218f81b960fb45668dbe7758aab99ce1
Danielred007 <danielrojo1927@gmail.com>
2024-02-10 06:43:37 +0000

    initial card component

---
+++ lab-02/src/pages/util/mock/data.mock.ts
@@ -0,0 +1,7 @@
+import { SelectCard } from "../models/select-card";
+
+
+export const selectCardMockData: SelectCard[] = [
+    {title: "Card 1", subtitle: "Card 1 Subtitle", usersNumber: 343},
+    {title: "Card 2", subtitle: "Card 2 Subtitle", usersNumber: 434}
+];
