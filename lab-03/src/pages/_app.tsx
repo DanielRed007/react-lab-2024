dd418c5289b0cfac79780f40aa430e220c570a3f
danielred007 <danielrojo1927@gmail.com>
2024-02-13 23:36:16 +0000

    adding lab-03, next-page-router, kinda cool :)

---
+++ lab-03/src/pages/_app.tsx
@@ -0,0 +1,19 @@
+import Footer from "@/components/footer/Footer";
+import Header from "@/components/header/Header";
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import Head from "next/head";
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>Your Website</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Header />
+      <Component {...pageProps} />
+      <Footer />
+    </>
+  );
+}
