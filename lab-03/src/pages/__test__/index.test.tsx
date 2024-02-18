8d6af4550ba3124d2c281d7f2dc9d34e52bb8ec7
danielred007 <danielrojo1927@gmail.com>
2024-02-18 13:57:29 +0000

    adding complete lab03 modal test cases

---
+++ lab-03/src/pages/__test__/index.test.tsx
@@ -0,0 +1,59 @@
+import "@testing-library/jest-dom";
+import { fireEvent, render, screen } from "@testing-library/react";
+import { LabContextProvider } from "@/context/LabContext";
+import Home from "../index";
+import Header from "@/components/header/Header";
+import DialogModal from "@/components/dialog-modal/DialogModal";
+import Footer from "@/components/footer/Footer";
+
+describe("Home", () => {
+  it("renders full homepage", () => {
+    render(
+      <LabContextProvider>
+        <Header />
+        <Home />
+        <Footer />
+        <DialogModal />
+      </LabContextProvider>
+    );
+
+    // Get Header content box and footer
+    const header = screen.getByText("Lab 03 - Next Page Router");
+    const body = screen.getByText("NextJS + Context API Workshop");
+    const footer = screen.getByText(
+      "© 2024 Your Website. All rights reserved."
+    );
+
+    // They must be rendered
+    expect(header).toBeInTheDocument();
+    expect(body).toBeInTheDocument();
+    expect(footer).toBeInTheDocument();
+  });
+
+  it("a payments modal can be activated and closed", async () => {
+    render(
+      <LabContextProvider>
+        <Header />
+        <Home />
+        <Footer />
+        <DialogModal />
+      </LabContextProvider>
+    );
+
+    // get payments button
+    const paymentsButton = screen.getByTestId("payments");
+    // click button
+    fireEvent.click(paymentsButton);
+
+    // modal container must appear
+    const dialogContainer = await screen.getByRole("modal-container");
+    expect(dialogContainer).toBeInTheDocument();
+
+    // get close modal button
+    const closeModalButton = await screen.getByRole("close-modal-button");
+    // click button
+    await fireEvent.click(closeModalButton);
+    // modal must disappear
+    expect(dialogContainer).not.toBeInTheDocument();
+  });
+});
