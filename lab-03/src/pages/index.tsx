aceecdb9bec9bffbce3b617442a2aafd525f2368
danielred007 <danielrojo1927@gmail.com>
2024-02-19 20:42:58 +0000

    adding dropdown selection structured component

--- lab-03/src/pages/index.tsx
+++ lab-03/src/pages/index.tsx
@@ -1,11 +1,22 @@
+import { useState, useEffect } from "react";
 import { useLabContext } from "@/context/LabContext";
 import ButtonCard from "@/components/button-card/ButtonCard";
+import Dropdown from "@/components/dropdown/Dropdown";
+import { mockWriters } from "@/util/mocks/mock-data";
+import { Writer } from "@/util/models/interfaces";
 
 export default function Index() {
-  const { openModal, modalType } = useLabContext();
+  const [dropdownOptionSelected, setDropdownOptionSelected] = useState(
+    mockWriters[0]
+  );
+  const { openModal } = useLabContext();
 
   const handleOpenModal = (type: string) => {
     openModal(type);
+  };
+
+  const handleDropdownOption = (option: Writer) => {
+    setDropdownOptionSelected(option);
   };
 
   return (
@@ -15,7 +26,8 @@ export default function Index() {
           NextJS + Context API Workshop
         </h2>
 
-        <div className="flex justify-center mt-10">
+        <div className="flex justify-center items-center mt-10">
+          <h2 className="text-2xl font-bold mb-4 mr-9">Modals</h2>
           <div className="max-w-xs w-48 h-48 rounded bg-yellow-500 overflow-hidden shadow-lg mx-4">
             <ButtonCard
               title="Payments"
@@ -28,6 +40,17 @@ export default function Index() {
               title="Form Sample"
               type="form"
               onClickButton={handleOpenModal}
+            />
+          </div>
+        </div>
+
+        <div className="flex justify-center items-center mt-10">
+          <h2 className="text-2xl font-bold mb-4 mr-9">Dropdown</h2>
+          <div className="flex justify-center items-center max-w-xs w-96 h-72 rounded bg-yellow-500 shadow-lg mx-4">
+            <Dropdown
+              options={mockWriters}
+              selectedOption={dropdownOptionSelected}
+              optionChange={handleDropdownOption}
             />
           </div>
         </div>
